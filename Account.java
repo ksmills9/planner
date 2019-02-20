c1584cded890af0808192ec7446904ee3b7342ac
Mushfik <ghostmushfik@gmail.com>
2019-02-19 17:27:36 -0700

    Constructor and getters

--- Account.java
+++ Account.java
@@ -7,4 +7,26 @@ public class Account {
     private TimeZone timeZone;
     private AllEvents allEvents;
 
+    Account(String name){
+        this.name = name;
+    }
+    Account(String name, String timezone){
+        this.name = name;
+        this.timeZone = TimeZone.getTimeZone(timezone);
+    }
+
+    Account(int ID, String name, String timezone, String events){
+        this.name = name;
+        this.timeZone = TimeZone.getTimeZone(timezone);
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTimeZone() {
+        return timeZone.toZoneId().toString();
+    }
+
 }
