d966c867d61bee920583abedf5bd988fd919f85e
nick98anderson <47461646+nick98anderson@users.noreply.github.com>
2019-04-08 18:04:33 -0600

    java doc

--- src/Reminder.java
+++ src/Reminder.java
@@ -1,13 +1,27 @@
 package src;
 
+
 public class Reminder extends Event{
-	
-	int ReminderID = 0;
-	
-	Reminder(int id, int userID, String name, String description, String start){
-		super(int id, userID, name, description, start);
-	
+
+	/**
+	*@param id The id of the reminder
+	*@param userID id assoicated with the user
+	*@param name The name of the reminder in string format
+	*@param start The start time of the reminder in string format
+	*/
+	Reminder(int id, int userID, String name, String start){
+		super(id, userID, name, start);
 	}
-	
+
+	/**
+	*@param id The id of the reminder
+	*@param userID id assoicated with the user
+	*@param name The name of the reminder in string format
+	*@param start The start time of the reminder in string format
+	*@param frequency How often the reminder occur. by default it is set to only occur once.
+	*/
+	Reminder(int id, int userID, String name, String start, String frequency){
+		super(id, userID, name, start, frequency);
+	}
 
 }
